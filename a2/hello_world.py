72c7163aaabd07060870acc6cb47b66c7241335e
ashleylynch <59947197+ashleylynch@users.noreply.github.com>
2020-01-17 14:12:01 -0500

    Create hello_world.py

---
+++ a2/hello_world.py
@@ -0,0 +1,18 @@
+"""
+Author: Ashley Lynch
+Date: 01/17/2020
+Goal: Hello world statement
+"""
+
+
+#declaring variable
+my_name = 'Ashley'
+my_age = 21
+my_hobby = 'playing video games'
+my_state = 'Alabama'
+
+
+print('Hello world, my name is ' + my_name)
+print(' - I am ' + str(my_age) + ' years old')
+print(' - I enjoy ' + my_hobby)
+print(' - I am from ' + my_state)
